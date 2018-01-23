c34b8457fa988c488d8c91e4e206296a5302b0aa
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2018-01-23 20:43:21 +0100

    require cubetools/cube-custom-fields-bundle (dev version), triggering the problem

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    CubeTools\CubeCustomFieldsBundle\CubeCustomFieldsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
